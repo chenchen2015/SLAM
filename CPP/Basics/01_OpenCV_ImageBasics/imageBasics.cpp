0ba8050d3540a609bd6545469984ed6d8af9b99d
Chen Chen <chenchen.bme@gmail.com>
2019-01-09 17:16:03 +0800

    completed basic OpenCV image operations 1

--- CPP/Basics/01_OpenCV_ImageBasics/imageBasics.cpp
+++ CPP/Basics/01_OpenCV_ImageBasics/imageBasics.cpp
@@ -1,14 +1,17 @@
 #include <chrono>
 #include <iostream>
+#include <string>
 using namespace std;
 
 // OpenCV
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+#define CV_WAIT cv::waitKey(0)
+constexpr char cCvWindow[] = "OpenCV Basics";
 enum SLAM_ERROR { ERR_FILE_NOT_EXIST = -1, ERR_FORMAT_NOT_SUPPORTED = -2 };
-
 using sClock = chrono::high_resolution_clock;
+
 int main(int argc, char** argv) {
     cv::Mat image;
 
@@ -20,8 +23,9 @@ int main(int argc, char** argv) {
         return SLAM_ERROR::ERR_FILE_NOT_EXIST;
     }
     // output basic info if successed
-    cv::imshow("test image", image);
-    cv::waitKey(0);
+    cv::namedWindow(cCvWindow, cv::WINDOW_AUTOSIZE);  // Create a window for display.
+    cv::imshow(cCvWindow, image);
+    CV_WAIT;
     // check image type
     if (image.type() != CV_8UC1 && image.type() != CV_8UC3) {
         // format not supported
@@ -46,8 +50,26 @@ int main(int argc, char** argv) {
         }
     }
     chrono::duration<double, std::milli> duration = sClock::now() - t0;
-    cout.recision(3);
-    cout << "Image iteration cost " << duration.count() << " ms" << endl;
+    cout.precision(3);
+    cout << "Iterate through entire image cost " << duration.count() << " ms"
+         << endl;
 
+    // copying cv::Mat
+    cv::Mat image_ref = image;  // image_ref is a reference of image
+    // change image_ref will cause both image_ref and image to change
+    auto rectTopLeft = cv::Rect(0, 0, 100, 100);
+    image_ref(rectTopLeft).setTo(0);  // set top left to 0
+    cv::imshow(cCvWindow, image);
+    CV_WAIT;
+
+    // use clone() to copy image data
+    cv::Mat image_clone = image.clone();
+    image_clone(rectTopLeft).setTo(255);
+    cv::imshow(cCvWindow, image);
+    cv::imshow(cCvWindow, image_clone);
+    CV_WAIT;
+
+    // clean up
+    cv::destroyAllWindows();
     return EXIT_SUCCESS;
 }
