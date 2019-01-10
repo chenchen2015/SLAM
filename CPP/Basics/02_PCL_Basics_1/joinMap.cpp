8baddcbb81b34a5bb95f68f03ef9ef9df88f9a25
Chen Chen <chenchen.bme@gmail.com>
2019-01-10 20:15:24 +0800

    updated pcl basics example

--- CPP/Basics/02_PCL_Basics_1/joinMap.cpp
+++ CPP/Basics/02_PCL_Basics_1/joinMap.cpp
@@ -52,13 +52,13 @@ int main(int argc, char** argv) {
         cerr << "Cannot open pose.txt, is it in the same directory?" << endl;
         return -1;
     }
-    boost::format fname_fmt("../%s%d.%s");  // filename pattern
+    boost::format fname_fmt("../%s/%d.%s");  // filename pattern
     for (int i = 0; i < 5; ++i) {
         // load color and depth images to stack
         cv::Mat colorImg =
             cv::imread((fname_fmt % "color" % (i + 1) % "png").str());
-        cv::Mat depthImg =
-            cv::imread((fname_fmt % "depth" % (i + 1) % "pgm").str(), -1);
+        cv::Mat depthImg = cv::imread(
+            (fname_fmt % "depth" % (i + 1) % "pgm").str(), -1);
         colorImgs.push_back(colorImg);
         depthImgs.push_back(depthImg);
         // load camera poses (Extrinsics)
@@ -70,10 +70,53 @@ int main(int argc, char** argv) {
 
     // create point cloud
     PointCloud::Ptr pointCloud(new PointCloud());
+    pointCloud->is_dense = false;  // not a dense point cloud
     for (int i = 0; i < 5; ++i) {
-        cout << "Converting image " << i + 1 << " ..." << endl;
+        cout << "Processing image " << i + 1 << " ..." << endl;
         cv::Mat colorImg = colorImgs[i];
-        cv::Mat;
+        cv::Mat depthImg = depthImgs[i];
+        Eigen::Isometry3d tr = poses[i];
+        // loop through image points
+        for (int v = 0; v < colorImg.rows; ++v) {
+            for (int u = 0; u < colorImg.cols; ++u) {
+                // get depth
+                unsigned int depth = depthImg.ptr<unsigned int>(v)[u];
+                // skip when depth == 0 which means not detected
+                if (!depth) continue;
+                // get point location [x, y, z]
+                // in camera coordinate frame
+                // d * [u, v, 1].T = K * p_c
+                // where K is the camera intrinsics matrix
+                // and p_c is the point position in
+                // camera coordinate frame
+                Eigen::Vector3d pointCam;
+                pointCam[2] = double(depth) / CamIntrinsics::depthScale;  // z
+                pointCam[0] = (u - CamIntrinsics::cx) * pointCam[2] /
+                              CamIntrinsics::fx;  // x
+                pointCam[1] = (v - CamIntrinsics::cy) * pointCam[2] /
+                              CamIntrinsics::fy;  // y
+                // get point location in world frame
+                // using camera extrinsics
+                Eigen::Vector3d pointWorld = tr * pointCam;
+                // assemble 3D point
+                PointT point;
+                point.x = pointWorld[0];
+                point.y = pointWorld[1];
+                point.z = pointWorld[2];
+                unsigned int colorChIdx =
+                    v * colorImg.step + u * colorImg.channels();
+                point.b = colorImg.data[colorChIdx];
+                point.g = colorImg.data[colorChIdx + 1];
+                point.r = colorImg.data[colorChIdx + 2];
+                // append point to point cloud
+                pointCloud->points.push_back(point);
+            }
+        }
     }
+    cout << "Processing done! Point cloud has total " 
+        << pointCloud->size() << " points" << endl;
+    // write point cloud to file
+    pcl::io::savePCDFileBinary("demo_map.pcd", *pointCloud);
+    
     return EXIT_SUCCESS;
 }
