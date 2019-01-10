f3226b5fc2c72a870ab82f2d6cd43725c2af1a4b
Chen Chen <chenchen.bme@gmail.com>
2019-01-10 10:44:00 +0800

    wip update

--- CPP/Basics/02_PCL_Basics_1/joinMap.cpp
+++ CPP/Basics/02_PCL_Basics_1/joinMap.cpp
@@ -22,6 +22,24 @@ struct CamIntrinsics {
     static constexpr double depthScale = 1000.0;
 };
 
+// Types
+using PointT = pcl::PointXYZRGB;
+using PointCloud = pcl::PointCloud<PointT>;
+
+Eigen::Isometry3d Post2Transform(double* const& pose) {
+    // combine translation and rotation specified from pose
+    // (camera extrinsics) into a transormation matrix
+    // pose = [x, y, z, qx, qy, qz, qw]
+
+    // Rotation
+    // Quaterniond constructor (qw, qx, qy, qz)
+    Eigen::Quaterniond quat(pose[6], pose[3], pose[4], pose[5]);
+    Eigen::Isometry3d tr(quat);
+    // Translation
+    tr.pretranslate(Eigen::Vector3d(pose[0], pose[1], pose[2]));
+    return tr;
+}
+
 int main(int argc, char** argv) {
     vector<cv::Mat> colorImgs, depthImgs;
     // camera poses
@@ -37,19 +55,25 @@ int main(int argc, char** argv) {
     boost::format fname_fmt("../%s%d.%s");  // filename pattern
     for (int i = 0; i < 5; ++i) {
         // load color and depth images to stack
-        cv::Mat colorImg = cv::imread((fname_fmt % "color" % (i + 1) % "png").str());
-        cv::Mat depthImg = cv::imread((fname_fmt % "depth" % (i + 1) % "pgm").str(), -1);
+        cv::Mat colorImg =
+            cv::imread((fname_fmt % "color" % (i + 1) % "png").str());
+        cv::Mat depthImg =
+            cv::imread((fname_fmt % "depth" % (i + 1) % "pgm").str(), -1);
         colorImgs.push_back(colorImg);
         depthImgs.push_back(depthImg);
         // load camera poses (Extrinsics)
+        // and convert to transformation matrices
         double camPose[7];
-        for (auto& n : camPose) fin >> n; // x, y, z, qx, qy, qz, qw
-        // Quaterniond constructor (qw, qx, qy, qz)
-        Eigen::Quaterniond quat(camPose[6], camPose[3], camPose[4], camPose[5]); 
-        Eigen::Isometry3d tr(quat);
-        tr.pretranslate(Eigen::Vector3d(camPose[0], camPose[1], camPose[2]));
-        poses.push_back(tr);
+        for (auto& n : camPose) fin >> n;  // x, y, z, qx, qy, qz, qw
+        poses.push_back(Post2Transform(camPose));
     }
 
+    // create point cloud
+    PointCloud::Ptr pointCloud(new PointCloud());
+    for (int i = 0; i < 5; ++i) {
+        cout << "Converting image " << i + 1 << " ..." << endl;
+        cv::Mat colorImg = colorImgs[i];
+        cv::Mat;
+    }
     return EXIT_SUCCESS;
 }
