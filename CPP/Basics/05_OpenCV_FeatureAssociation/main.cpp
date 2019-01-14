2706e7323b88af14ac99d65f80ba69aadc279969
Chen Chen <chenchen.bme@gmail.com>
2019-01-14 20:22:11 +0800

    05 feature association: working version

--- CPP/Basics/05_OpenCV_FeatureAssociation/main.cpp
+++ CPP/Basics/05_OpenCV_FeatureAssociation/main.cpp
@@ -5,7 +5,15 @@ using namespace std;
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-constexpr char[] cWindowName = "main";
+constexpr char cWindowMain[] = "main";
+#define CV_WAIT cv::waitKey(0)
+
+// show image in the main window with title
+inline void showImage(const cv::Mat& img, const string& title = "") {
+    if (title.size()) cv::setWindowTitle(cWindowMain, title);
+    cv::imshow(cWindowMain, img);
+    CV_WAIT;
+}
 
 int main(int argc, char** argv) {
     // read images
@@ -35,6 +43,46 @@ int main(int argc, char** argv) {
     // visualize key points
     cv::Mat orbImg1;
     cv::drawKeypoints(img1, keyPoints1, orbImg1);
+    cv::namedWindow(cWindowMain, cv::WINDOW_AUTOSIZE);
+    // cv::setWindowTitle(cWindowMain, "ORB features");
+    // cv::imshow(cWindowMain, orbImg1);
+    showImage(orbImg1, "ORB Features");
+
+    // match detected features
+    vector<cv::DMatch> rawMatches;
+    // matcher using Hamming distance
+    // since ORB features yields binary descriptor
+    cv::BFMatcher matcher(cv::NORM_HAMMING);
+    matcher.match(descriptors1, descriptors2, rawMatches);
+
+    // filter matched features
+    double minDist = 10000.0, maxDist = 0.0;
+    // compute min and max distance among matched features
+    for (const auto& feat : rawMatches) {
+        if (feat.distance < minDist) minDist = feat.distance;
+        if (feat.distance > maxDist) maxDist = feat.distance;
+    }
+    cout << "Min and max distance: " << minDist << ", " << maxDist << endl;
+    // filter matched features based on distance
+    vector<cv::DMatch> goodMatches;
+    // minimum threshold to qualify as good match
+    double distThreshold = max(2*minDist, 20.0);
+    for (const auto& feat : rawMatches) {
+        if (feat.distance <= distThreshold) {
+            goodMatches.push_back(feat);
+        }
+    }
+    printf("Found %lud raw feature matches and %lu good matches\n",
+           rawMatches.size(), goodMatches.size());
+
+    // visualize matched result
+    cv::Mat imgRawMatch, imgGoodMatch;
+    cv::drawMatches(img1, keyPoints1, img2, keyPoints2, rawMatches,
+                    imgRawMatch);
+    cv::drawMatches(img1, keyPoints1, img2, keyPoints2, goodMatches,
+                    imgGoodMatch);
+    showImage(imgRawMatch, "Raw feature match");
+    showImage(imgGoodMatch, "Filtered feature match");
 
     return EXIT_SUCCESS;
 }
