49332ff146bd225885bebfe26f35c4722c36346b
Chen Chen <chenchen.bme@gmail.com>
2019-01-16 15:29:01 +0800

    working version of 06 pose estimation

--- CPP/Basics/06_OpenCV_PoseEstimation/main.cpp
+++ CPP/Basics/06_OpenCV_PoseEstimation/main.cpp
@@ -11,12 +11,12 @@ constexpr char cWindowMain[] = "main";
 
 // camera model
 namespace TUMCamera {
-// camera intrinsic matrix, TUM Freiburg2
-const cv::Mat K =
-    (cv::Mat_<double>(3, 3) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1);
-// camera parameters
-const cv::Point2d principalPt(325.1, 249.7);
-constexpr double focalLen = 521;
+    // camera intrinsic matrix, TUM Freiburg2
+    const cv::Mat K =
+        (cv::Mat_<double>(3, 3) << 520.9, 0, 325.1, 0, 521.0, 249.7, 0, 0, 1);
+    // camera parameters
+    const cv::Point2d principalPt(325.1, 249.7);
+    constexpr double focalLen = 521;
 };  // namespace TUMCamera
 
 // show image in the main window with title
@@ -34,8 +34,8 @@ void findFeatureMatches(const cv::Mat img1, const cv::Mat& img2,
 
 void poseEstimation2d2d(const vector<cv::KeyPoint>& keyPoints1,
                         const vector<cv::KeyPoint>& keyPoints2,
-                        const vector<cv::DMatch>& matches, cv::Mat Rot,
-                        cv::Mat t);
+                        const vector<cv::DMatch>& matches, cv::Mat& Rot,
+                        cv::Mat& t);
 
 // image pixel coordinate to camera coordinates
 cv::Point2d img2Cam(const cv::Point2d& p, const cv::Mat& K);
@@ -50,6 +50,29 @@ int main(int argc, char** argv) {
     vector<cv::DMatch> matches;
     findFeatureMatches(img1, img2, keyPoints1, keyPoints2, matches);
     printf("Found %lu matched feature points", matches.size());
+    // estimate camera pose
+    cv::Mat Rot, t;
+    poseEstimation2d2d(keyPoints1, keyPoints2, matches, Rot, t);
+    // validate EssentialMatrix = t ^ Rot * scale
+    cv::Mat tHat =
+        (cv::Mat_<double>(3, 3) << 0, -t.at<double>(2, 0), t.at<double>(1, 0),
+         t.at<double>(2, 0), 0, -t.at<double>(0, 0), -t.at<double>(1, 0),
+         t.at<double>(0, 0), 0);
+    cout << "t ^ R = " << endl << tHat * Rot << endl;
+    // validate epipolar constraint
+    double maxConstraint = 0;
+    for (const auto& m : matches) {
+        cv::Point2d pt1 = img2Cam(keyPoints1[m.queryIdx].pt, TUMCamera::K);
+        cv::Point2d pt2 = img2Cam(keyPoints2[m.trainIdx].pt, TUMCamera::K);
+        // convert to homogenous coordinates
+        cv::Mat y1 = (cv::Mat_<double>(3, 1) << pt1.x, pt1.y, 1);
+        cv::Mat y2 = (cv::Mat_<double>(3, 1) << pt2.x, pt2.y, 1);
+        // epipolar constraint
+        cv::Mat d = y2.t() * tHat * Rot * y1;
+        if(maxConstraint < d.at<double>(0,0))
+            maxConstraint = d.at<double>(0, 0);
+    }
+    cout << "epipolar constaint (max) = " << maxConstraint << endl;
 
     return EXIT_SUCCESS;
 }
@@ -60,16 +83,26 @@ void findFeatureMatches(const cv::Mat img1, const cv::Mat& img2,
                         vector<cv::DMatch>& matches) {
     // feature descriptors
     cv::Mat desc1, desc2;
-    cv::Ptr<cv::FeatureDetector> detector = cv::ORB::create();
-    cv::Ptr<cv::DescriptorExtractor> descriptor = cv::ORB::create();
+    cv::Ptr<cv::ORB> orb = cv::ORB::create(
+        500,   // maximum number of features to retain
+        1.2f,  // Pyramid decimation ratio, similar to mip-mapping
+        8,     // number of pyramid levels
+        31,    // size of the border where the features are not detected
+        0,     // first level: level of pyramid to put source image to
+        2,     // WTA_K: number of points that produce each element of the
+               // oriented BRIEF descriptor
+        cv::ORB::HARRIS_SCORE,  // algorithm to rank features
+        31,                     // size of the patch used by the oriented BRIEF
+        20                      // fast threshold
+    );
     cv::Ptr<cv::DescriptorMatcher> matcher =
         cv::DescriptorMatcher::create("BruteForce-Hamming");
     // detect oriented FAST features
-    detector->detect(img1, keyPoints1);
-    detector->detect(img2, keyPoints2);
+    orb->detect(img1, keyPoints1);
+    orb->detect(img2, keyPoints2);
     // extract BRIEF descriptors
-    descriptor->compute(img1, keyPoints1, desc1);
-    descriptor->compute(img2, keyPoints2, desc2);
+    orb->compute(img1, keyPoints1, desc1);
+    orb->compute(img2, keyPoints2, desc2);
     // match features based on Hamming distance
     vector<cv::DMatch> rawMatches;
     matcher->match(desc1, desc2, rawMatches);
@@ -88,7 +121,7 @@ void findFeatureMatches(const cv::Mat img1, const cv::Mat& img2,
             matches.push_back(match);
         }
     }
-    printf("Found %lud raw feature matches and %lu good matches\n",
+    printf("Found %lu raw feature matches and %lu good matches\n",
            rawMatches.size(), matches.size());
 }
 
@@ -100,8 +133,8 @@ cv::Point2d img2Cam(const cv::Point2d& p, const cv::Mat& K) {
 
 void poseEstimation2d2d(const vector<cv::KeyPoint>& keyPoints1,
                         const vector<cv::KeyPoint>& keyPoints2,
-                        const vector<cv::DMatch>& matches, cv::Mat Rot,
-                        cv::Mat t) {
+                        const vector<cv::DMatch>& matches, 
+                        cv::Mat& Rot, cv::Mat& t) {
     // convert matched key points to vector of Point2f
     vector<cv::Point2f> pts1, pts2;
     for (int i = 0; i < matches.size(); ++i) {
