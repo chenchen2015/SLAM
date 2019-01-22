4525723c91d09bfa27c1e56e52a1ea1a32d767d2
Chen Chen <chenchen.bme@gmail.com>
2019-01-22 21:30:29 +0800

    initial commit xSLAM VO frontend

---
+++ CPP/Project/v0.1/include/xslam/xslam.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// Eigen
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+using Eigen::Vector2d;
+using Eigen::Vector3d;
+// Sophus
+#include <sophus/se3.h>
+using SE3 = Sophus::SE3d;
+// OpenCV
+#include <opencv2/core/core.hpp>
+using cv::Mat;
+// STL
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
