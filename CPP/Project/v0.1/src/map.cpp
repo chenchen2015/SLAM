d7c02a897aefceb9b44987ddacab5aefa19431a7
Chen Chen <chenchen.bme@gmail.com>
2019-01-23 23:08:21 +0800

    xSLAM - add map source

---
+++ CPP/Project/v0.1/src/map.cpp
@@ -0,0 +1,22 @@
+#include <xslam/map.h>
+
+namespace xslam {
+
+void Map::insertKeyFrame(Frame::Ptr frame) {
+    cout << "Key frame size = " << keyframes_.size() << endl;
+    if (keyframes_.find(frame->id_) == keyframes_.end()) {
+        keyframes_.insert({frame->id_, frame});
+    } else {
+        keyframes_[frame->id_] = frame;
+    }
+}
+
+void Map::insertMapPoint(MapPoint::Ptr mp) {
+    if (mapPoints_.find(mp->id_) == mapPoints_.end()) {
+        mapPoints_.insert({mp->id_, mp});
+    } else {
+        mapPoints_[mp->id_] = mp;
+    }
+}
+
+}  // namespace xslam
