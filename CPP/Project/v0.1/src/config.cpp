eab988805ca21a8a04b53f22099533db33fa4e54
Chen Chen <chenchen.bme@gmail.com>
2019-01-24 17:56:46 +0800

    xSLAM - added config src

---
+++ CPP/Project/v0.1/src/config.cpp
@@ -0,0 +1,22 @@
+#include <xslam/config.h>
+
+namespace xslam {
+
+shared_ptr<Config> Config::config_ = nullptr;
+
+void Config::setParameterFile(const std::string& filename) {
+    if (!config_) config_ = shared_ptr<Config>(new Config);
+    config_->file_ = cv::FileStorage(filename.c_str(), cv::FileStorage::READ);
+    if (config_->file_.isOpened() == false) {
+        std::cerr << "parameter file " << filename << " does not exist."
+                  << std::endl;
+        config_->file_.release();
+        return;
+    }
+}
+
+Config::~Config() {
+    if (file_.isOpened()) file_.release();
+}
+
+}  // namespace xslam
