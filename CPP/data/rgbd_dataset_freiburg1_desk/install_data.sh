c91ad8876400cd9e7c15046734c0e393379e636b
Chen Chen <chenchen.bme@gmail.com>
2019-02-19 20:51:44 -0600

    removed bin from project folder, updated data install script to use axel for speeding up and added data association call

--- CPP/data/rgbd_dataset_freiburg1_desk/install_data.sh
+++ CPP/data/rgbd_dataset_freiburg1_desk/install_data.sh
@@ -1,3 +1,5 @@
 cd ..
-wget https://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_desk.tgz
+axel -an 8 https://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_desk.tgz -o rgbd_freiburg1_desk.tgz
 tar -xf rgbd*.tgz
+cd rgbd_dataset*
+python3 associate.py rgb.txt depth.txt > associate.txt
