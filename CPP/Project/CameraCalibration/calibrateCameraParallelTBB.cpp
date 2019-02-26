42d1978d182095b368a856308bcd1de473c5ef79
Chen Chen <chenchen.bme@gmail.com>
2019-02-26 13:57:17 -0600

    wip - adding circle pattern code

--- CPP/Project/CameraCalibration/calibrateCameraParallelTBB.cpp
+++ CPP/Project/CameraCalibration/calibrateCameraParallelTBB.cpp
@@ -12,14 +12,34 @@ using namespace cv;
 
 #define CV_WAIT waitKey(0);
 constexpr char cvMainWindow[] = "OpenCV Camera Calibration";
-constexpr int boardW = 7; // 7 corners in width
-constexpr int boardH = 9; // 9 corners in height
-constexpr int chessboardFlags = CALIB_CB_ADAPTIVE_THRESH |
-                                /*CALIB_CB_NORMALIZE_IMAGE |*/
-                                CALIB_CB_FAST_CHECK;
-Size patternSize(boardW, boardH); // board corner count
 TermCriteria termCriteria(TermCriteria::EPS + TermCriteria::COUNT, 50,
                           0.0000001);
+
+// calibration data source info struct
+enum CalibrationPatternType { CHESSBOARD_PATTERN,
+                              CIRCLE_PATTERN };
+struct CalibrationSrcImgData {
+    // pattern type
+    const CalibrationPatternType patternType;
+    // src image path
+    const char *srcPath;
+    // pattern size
+    const Size patternSize;
+    const int flags;
+    // pattern reference size
+    // width of the square if CHESSBOARD_PATTERN
+    // diameter of the circle when CIRCLE_PATTERN
+    // in unit [mm]
+    const float refSize;
+};
+CalibrationSrcImgData chessboardDataset{
+    CalibrationPatternType::CHESSBOARD_PATTERN,
+    "../SamsungS8Plus/Chessboard1/*.jpg", Size(7, 9),
+    CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FAST_CHECK, 20};
+CalibrationSrcImgData circleGridDataset{CalibrationPatternType::CIRCLE_PATTERN,
+                                        "../SamsungS8Plus/CirclePattern1/*.jpg",
+                                        Size(4, 11), CALIB_CB_ASYMMETRIC_GRID,
+                                        18};
 
 // log function
 template <typename... Args>
@@ -31,8 +51,9 @@ inline void logMsg(const char *fmt, Args... args) {
 
 // process a single image to extract corners
 bool processCalibrateImage(const string &filename,
-                           vector<vector<Point2f>> &imgCorners,
-                           bool useSubPix = true, bool visualize = false) {
+                           CalibrationSrcImgData *pImgData,
+                           vector<vector<Point2f>> &imgPts,
+                           bool visualize = false) {
     logMsg("processing %s", filename.c_str());
     // load image
     Mat image;
@@ -45,49 +66,54 @@ bool processCalibrateImage(const string &filename,
     // threshold(image, image, 120, 255, THRESH_BINARY);
     // [Optional] downsample image for faster processing
     // resize(image, image, Size(), 0.25, 0.25);
+    vector<Point2f> pts2d;
+    switch (pImgData->patternType) {
+    case CalibrationPatternType::CHESSBOARD_PATTERN:
+        // detect chessboard corners
+        // https://docs.opencv.org/4.0.1/d9/d0c/group__calib3d.html#ga93efa9b0aa890de240ca32b11253dd4a
+        bool valid = findChessboardCorners(image, pImgData->patternSize, pts2d,
+                                           pImgData->flags);
+        if (!valid) {
+            logMsg("%s", "feature point detection failed!");
+            return false;
+        }
+        logMsg("found %ld feature points", pts2d.size());
+        // use cornerSubPix to refine the detected corders
+        cornerSubPix(image, pts2d, Size(11, 11), Size(-1, -1), termCriteria);
+        imgPts.push_back(pts2d);
+        // [Optional] visuzlize
+        if (visualize) {
+            drawChessboardCorners(image, pImgData->patternSize, Mat(pts2d), valid);
+            // show image with detected feature points
+            namedWindow(cvMainWindow, WINDOW_NORMAL);
+            imshow(cvMainWindow, image);
+            CV_WAIT;
+        }
+        break;
+        //case CalibrationPatternType::CIRCLE_PATTERN:
+    }
 
-    // detect chessboard corners
-    // https://docs.opencv.org/4.0.1/d9/d0c/group__calib3d.html#ga93efa9b0aa890de240ca32b11253dd4a
-    vector<Point2f> corners;
-    // new candidate to try findChessboardCornersSB
-    // bool valid =
-    //     findChessboardCornersSB(image, patternSize, corners,
-    //     CALIB_CB_EXHAUSTIVE);
-    bool valid =
-        findChessboardCorners(image, patternSize, corners, chessboardFlags);
-    if (!valid) {
-        logMsg("%s", "corner detection failed!");
-        return false;
-    }
-    logMsg("found %ld corners", corners.size());
-
-    // [Optional] use cornerSubPix to refine the detected corders
-    if (useSubPix) {
-        // Mat gray;
-        cornerSubPix(image, corners, Size(11, 11), Size(-1, -1), termCriteria);
-    }
-    imgCorners.push_back(corners);
-    // [Optional] visuzlize
-    if (visualize) {
-        drawChessboardCorners(image, patternSize, Mat(corners), valid);
-        // show image with detected corners
-        namedWindow(cvMainWindow, WINDOW_NORMAL);
-        imshow(cvMainWindow, image);
-        CV_WAIT;
-    }
     return true;
 }
 
-// calculate reference 3D coordinates for chessboard corners
+// calculate reference 3D coordinates of the pattern
 // the z-value is 0 since the corner points are coplanar
-void calcChessboardCorners(float squareSize, int imgCnt,
-                           vector<vector<Point3f>> &patternCorners) {
-    vector<Point3f> corners;
-    for (int i = 0; i < boardH; i++)
-        for (int j = 0; j < boardW; j++)
-            corners.push_back(
-                Point3f(float(j * squareSize), float(i * squareSize), 0));
-    patternCorners = vector<vector<Point3f>>(imgCnt, corners);
+void calcRefPatternPts(CalibrationSrcImgData *pImgData, int imgCnt,
+                       vector<vector<Point3f>> &patternPts) {
+    vector<Point3f> pt3d;
+    switch (pImgData->patternType) {
+    case CalibrationPatternType::CHESSBOARD_PATTERN:
+        for (int i = 0; i < pImgData->patternSize.height; i++)
+            for (int j = 0; j < pImgData->patternSize.width; j++)
+                pt3d.push_back(Point3f(float(j * pImgData->refSize),
+                                       float(i * pImgData->refSize), 0));
+        patternPts = vector<vector<Point3f>>(imgCnt, pt3d);
+        patternPts[0][pImgData->patternSize.width - 1].x =
+            patternPts[0][0].x +
+            (pImgData->refSize * (pImgData->patternSize.width - 1));
+        patternPts.resize(patternPts.size(), patternPts[0]);
+        break;
+    }
 }
 
 // compute reprojection error
@@ -116,19 +142,27 @@ double computeReprojectionErrors(const vector<vector<Point3f>> &patternPoints,
 }
 
 // TODO: add distortion removal
+// void unDistortImage(Mat &cameraMatrix, Mat &distCoeffs) {
+//     Mat view, rview, map1, map2;
+//     initUndistortRectifyMap(cameraMatrix, distCoeffs, Mat(),
+//                             getOptimalNewCameraMatrix(cameraMatrix,
+//                             distCoeffs,
+//                                                       patternSize, 1,
+//                                                       patternSize, 0),
+//                             patternSize, CV_16SC2, map1, map2);
+// }
 
 int main(int argc, char **argv) {
+    CalibrationSrcImgData *pImgData = &chessboardDataset;
     // get input image list
     vector<string> images;
-    glob("../S8plus/*.jpg", images);
+    glob("../SamsungS8Plus/Chessboard1/*.jpg", images);
     logMsg("found %ld images for calibration", images.size());
     // extract corner points
-    vector<vector<Point2f>> imgCorners;
-    vector<vector<Point3f>> patternCorners;
-    calcChessboardCorners(20, images.size(), patternCorners);
-    patternCorners[0][boardW - 1].x =
-        patternCorners[0][0].x + (20 * (boardW - 1));
-    patternCorners.resize(patternCorners.size(), patternCorners[0]);
+    vector<vector<Point2f>> imgPts;
+    vector<vector<Point3f>> patternPts;
+    calcRefPatternPts(pImgData, images.size(), patternPts);
+
     // use parallel (need to compile OpenCV with TBB)
     // in C++11 fashion
     // example:
@@ -136,30 +170,30 @@ int main(int argc, char **argv) {
     // setNumThreads(8);
     parallel_for_(Range(0, images.size()), [&](const Range &range) {
         for (int i = range.start; i < range.end; ++i) {
-            processCalibrateImage(images[i], imgCorners);
+            processCalibrateImage(images[i], pImgData, imgPts);
         }
     });
     // initialize output data container
     Mat distCoeffs = Mat::zeros(8, 1, CV_64F);
     Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
     vector<Mat> rVecs, tVecs;
-    vector<Point3f> newObjPoints = patternCorners[0];
+    vector<Point3f> newObjPts = patternPts[0];
     double rms;
     rms = calibrateCameraRO(
-        patternCorners, imgCorners, patternSize, boardW - 1, cameraMatrix,
-        distCoeffs, rVecs, tVecs, newObjPoints,
+        patternPts, imgPts, pImgData->patternSize,
+        pImgData->patternSize.width - 1, cameraMatrix, distCoeffs, rVecs, tVecs,
+        newObjPts,
         CALIB_USE_LU); // CALIB_TILTED_MODEL,
                        // CALIB_THIN_PRISM_MODEL,
                        // CALIB_RATIONAL_MODEL
                        // | CALIB_TILTED_MODEL | CALIB_RATIONAL_MODEL |
                        // CALIB_THIN_PRISM_MODEL
-
     logMsg("RMS error reported by calibrateCamera %g", rms);
     // evaluate reprojection error
     vector<float> perViewErrors;
     double rmsReproj =
-        computeReprojectionErrors(patternCorners, imgCorners, rVecs, tVecs,
-                                  cameraMatrix, distCoeffs, perViewErrors);
+        computeReprojectionErrors(patternPts, imgPts, rVecs, tVecs, cameraMatrix,
+                                  distCoeffs, perViewErrors);
     logMsg("RMS reprojection error %g", rmsReproj);
     logMsg("%s", "Per view reprojection error: ");
     for (int i = 0; i < perViewErrors.size(); ++i)
